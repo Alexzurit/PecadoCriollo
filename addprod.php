b4b99685e2f492dad1bfb0f19d56432238bcc750
Eduardo <alexeduardo.zuritajulca.3@gmail.com>
2024-03-27 17:19:01 -0500

    Todo listo para crear el sistema
    siuuuuu

---
+++ addprod.php
@@ -0,0 +1,5 @@
+<?php require_once 'vistas/parte-superior.php'; ?>
+<!-- ========= Main content section of dashboard ======= -->
+
+
+<?php require_once 'vistas/parte-inferior.php'; ?>            
---
+++ vistas/parte-inferior.php
@@ -0,0 +1,45 @@
+<!-- ========= light and dark mode toggle button ======= -->
+
+            <a href="#" class="theme-toggle">
+                <i class="fa-regular fa-moon"></i>
+                <i class="fa-regular fa-sun"></i>
+            </a>
+
+            <!-- ========= footer section of dashboard ======= -->
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row text-muted">
+                        <div class="col-6 text-start">
+                            <p class="mb-0">
+                                <a href="#" class="text-muted">
+                                    <strong>CodzWod</strong>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="col-6 text-end">
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <a href="" class="text-muted">Contact</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="" class="text-muted">About us</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="" class="text-muted">Terms</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="" class="text-muted">Booking</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
